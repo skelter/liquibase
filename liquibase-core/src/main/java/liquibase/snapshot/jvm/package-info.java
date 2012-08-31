f270e5f3d55538877b9d6bde6519099927dfd98e
Steve Suehs <suehs@datical.com>
2012-08-31 13:11:06 -0500

    Document the jvm package

---
+++ liquibase-core/src/main/java/liquibase/snapshot/jvm/package-info.java
@@ -0,0 +1,9 @@
+package liquibase.snapshot.jvm;
+
+/**
+ * Vendor-specific implementations of the DatabaseSnapshotGenerator interface as well as the
+ * abstract class JdbcDatabaseSnapshotGenerator which contains much of the generator logic.
+ *
+ * @see liquibase.snapshot.DatabaseSnapshotGenerator
+ * @see liquibase.snapshot.jvm.JdbcDatabaseSnapshotGenerator
+ */
